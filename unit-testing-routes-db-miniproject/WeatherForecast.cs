732f36254d8c306fba23c40dd8a7f24246f41e7c
nathanjh-28 <nathan@nathanjharris.com>
2025-06-28 14:35:19 -0400

    initial commit xUnit tests and api

---
+++ unit-testing-routes-db-miniproject/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace unit_testing_routes_db_miniproject;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
