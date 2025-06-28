a1996d4dad97ff73cd5a99bea7cd2be91d9b3027
nathanjh-28 <nathan@nathanjharris.com>
2025-06-28 14:55:28 -0400

    refactored for controller based api

--- unit-testing-routes-db-miniproject/Program.cs
+++ unit-testing-routes-db-miniproject/Program.cs
@@ -1,4 +1,3 @@
-
 namespace unit_testing_routes_db_miniproject;
 
 public class Program
@@ -8,6 +7,7 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+        builder.Services.AddControllers();
         builder.Services.AddAuthorization();
 
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -27,25 +27,8 @@ public class Program
 
         app.UseAuthorization();
 
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        app.MapControllers();
 
-        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-        {
-            var forecast =  Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                {
-                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    TemperatureC = Random.Shared.Next(-20, 55),
-                    Summary = summaries[Random.Shared.Next(summaries.Length)]
-                })
-                .ToArray();
-            return forecast;
-        })
-        .WithName("GetWeatherForecast")
-        .WithOpenApi();
 
         app.Run();
     }
